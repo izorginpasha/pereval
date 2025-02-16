817e80a3b8074a5e7d7d2b082abfdd08ff7ee312
izorgin.pasha <izorgin.pasha@yandex.ru>
2025-02-16 18:27:11 +0500

    add_valid_eroor_exception_handler

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 
 import uvicorn
 from src.core.config import uvicorn_options
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException, Request
 from sqlalchemy import text
 from src.api import api_router
-
-
+from pydantic import ValidationError
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 
 
@@ -13,7 +14,21 @@ from src.api import api_router
 app = FastAPI(
     docs_url="/api/openapi"
 )
+# Глобальный обработчик ошибок валидации (422 Unprocessable Entity)
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,  # Меняем стандартный 422 на 400
+        content={"detail": "Некорректные данные. Проверьте ввод и повторите попытку."}
+    )
 
+# Глобальный обработчик любых других исключений
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": "Произошла ошибка. Проверьте ввод и повторите попытку."}
+    )
 app.include_router(api_router)
 
 if __name__ == '__main__':
