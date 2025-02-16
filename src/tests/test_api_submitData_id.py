c56d74958c5516859ae3b8a2dd94ee1545d7e662
izorgin.pasha <izorgin.pasha@yandex.ru>
2025-02-16 18:22:54 +0500

    add_test_api_submitData_id

---
+++ src/tests/test_api_submitData_id.py
@@ -0,0 +1,24 @@
+import pytest
+import httpx
+from unittest.mock import patch, AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_create_valid_pereval(load_test_data):
+    """Тест успешной отправки валидных данных"""
+    pereval_data = load_test_data["valid_pereval_id"]
+
+
+    # Мокируем метод `get` класса `AsyncClient` из `httpx`
+    async def mock_get(*args, **kwargs):
+        mock_response = AsyncMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = pereval_data
+        return mock_response
+
+    with patch('httpx.AsyncClient.get', new=mock_get):
+        async with httpx.AsyncClient() as ac:
+            response = await ac.get("/submitData/23")
+        assert response.status_code == 200
+        response_json = await response.json()
+        assert response_json == pereval_data
